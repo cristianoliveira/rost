d2d0d4ebc67863d6a1b468c8f999044f0c90218f
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-03-19 01:32:08 -0300

    feat: better exception handling

--- src/commands.rs
+++ src/commands.rs
@@ -1,49 +1,59 @@
 use std::io::prelude::*;
 use std::fs::File;
+use std::io::BufReader;
+use std::io::SeekFrom;
+use std::io;
 
-pub trait Command {
-  fn execute(&self, file: File) -> bool;
+pub trait Execution {
+  fn execute(&self, file: File) -> io::Result<()>;
 }
 
 #[derive(Debug)]
 pub struct AddCommand {
-    pub args: Vec<String>,
+    pub ip: String,
+    pub host: String,
 }
 
-impl Command for AddCommand {
-    fn execute(&self, mut file: File) -> bool {
-       let ip = self.args[2].to_string();
-       let host = self.args[3].to_string();
-       let new_item = format!("{ip}  {host}", ip=ip, host=host);
-       let bytes = match file.write(new_item.as_ref()) {
-           Ok(b) => b,
-           Err(err) => panic!("file error: {}", err)
-       };
-       return true;
+impl Execution for AddCommand {
+    fn execute(&self, mut file: File) -> io::Result<()> {
+       let new_item = format!("\n {ip}  {host} #r", ip=self.ip, host=self.host);
+       try!(file.write(new_item.as_ref()));
+       Ok(())
     }
 }
 
 pub struct DeleteCommand {
-    pub args: Vec<String>,
+    pub host_ip: String,
 }
 
-impl Command for DeleteCommand {
-    fn execute(&self, file: File) -> bool {
-      println!("Command not implemented");
-      return true;
+impl Execution for DeleteCommand {
+    fn execute(&self, mut file: File) -> io::Result<()> {
+      let mut content = String::new();
+      file.read_to_string(&mut content);
+
+      let mut new_content = String::new();
+      for line in content.lines() {
+        if !line.contains(&self.host_ip){
+          new_content.push_str(line);
+          new_content.push_str("\n");
+        }
+      };
+
+      file.set_len(0);
+      file.write_all(new_content.as_ref())
     }
 }
 
-pub fn execute(command: String, args: Vec<String>) -> bool {
-    let mut file = match File::open("/etc/hosts") {
-        Ok(f) => f,
-        Err(err) => panic!("file error: {}", err)
+pub fn make(command: String, args: Vec<String>) -> Option<Box<Execution+'static>>{
+    let cmd: Box<Execution> = match command.as_ref() {
+        "add" => Box::new(AddCommand{
+            ip: args[2].to_string(),
+            host: args[3].to_string(),
+        }),
+        "delete" => Box::new(DeleteCommand{
+            host_ip: args[2].to_string(),
+        }),
+         _  => return None
     };
-
-    match command.as_ref() {
-        "add" => { AddCommand{ args: args }.execute(file); },
-        "delete" => { DeleteCommand{ args: args }.execute(file); },
-         _  => return false
-    };
-    return true;
+    return Some(cmd);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::{File,OpenOptions};
+use std::io::prelude::*;
+
 use std::env;
 mod commands;
 
@@ -14,11 +17,22 @@ fn die_showing_help() -> ! {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut file = match OpenOptions::new().append(true)
+                                           .read(true)
+                                           .write(true)
+                                           .open("/etc/hosts_bkp") {
+        Ok(f) => f,
+        Err(err) => panic!("file error: {}", err)
+    };
 
     let command = args[1].to_string();
-    if commands::execute(command, args) {
-        println!("Command executed.");
-    } else {
-        die_showing_help();
+    let execution = match commands::make(command, args) {
+      Some(e) => e ,
+      None => { die_showing_help(); },
+    };
+
+    match execution.execute(file) {
+      Ok(_) => { println!("Host changed with success.")},
+      Err(err) => panic!("Error while execution: {}", err),
     }
 }
